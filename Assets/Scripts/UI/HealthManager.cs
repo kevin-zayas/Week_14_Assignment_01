eb8b56810097358a034f095f9e7dd6bd90449a54
Kevin Zayas <Zayas.kevin1999@gmail.com>
2024-01-30 17:23:51 -0500

    Added new Game Feel Concept to prototype
    Added a post processing layer that flashes red whenever the player's health decreases (by pressing the down arrow)

--- Assets/Scripts/UI/HealthManager.cs
+++ Assets/Scripts/UI/HealthManager.cs
@@ -2,29 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class HealthManager : MonoBehaviour
 {
 
+    public float intensity = 0;
     public Image healthBar;
     public float healthAmount = 100f;
+
+    PostProcessVolume _volume;
+    Vignette _vignette;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _volume = GetComponent<PostProcessVolume>();
+        _volume.profile.TryGetSettings<Vignette>(out _vignette);
+
+        if (!_vignette)
+        {
+            print("error, vignette empty");
+        }
+
+        else
+        {
+            _vignette.enabled.Override(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             Heal(10);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             TakeDamage(20);
+            StartCoroutine(TakeDamageEffect());
         }
+    }
+
+    public IEnumerator TakeDamageEffect()
+    {
+        intensity = 0.4f;
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(0.4f);
+
+        yield return new WaitForSeconds(0.4f);
+
+        while (intensity > 0)
+        {
+            intensity -= 0.01f;
+
+            if (intensity < 0) intensity = 0;
+
+            _vignette.intensity.Override(intensity);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _vignette.enabled.Override(false);
+        yield break;
     }
 
     public void TakeDamage(float damage)
